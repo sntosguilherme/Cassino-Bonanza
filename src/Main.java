27fb44772ae17d56fe2c06d8180fb4fadb7f8657
unknown <sntos.guilherme9705@gmail.com>
2025-10-03 22:02:36 -0300

    adicionado slotgame e recursos

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("oii");
-        Ranking.mostrarTop10();
     }
     
 }
