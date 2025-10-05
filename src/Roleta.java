c97360add0e1e1143e27d5d0cc2883a60dbcd331
LaraGGomes <laragomes471@gmail.com>
2025-10-05 16:09:36 -0300

    interface e lógica da roleta atualizada

--- src/Roleta.java
+++ src/Roleta.java
@@ -5,10 +5,12 @@ import java.security.SecureRandom;      // Gerador de números aleatórioss
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class Roleta extends JPanel {
-    private final JLabel resultado; // Mídia que pode ser inserida depois (texto, imagem) do swing
-    private final JButton girar;
+    private Jogador j;
+    private double apostaMontante;
 
     private static final int[] numeros = {
             0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24,
@@ -31,20 +33,44 @@ public class Roleta extends JPanel {
     private final Timer timer;
 
     private void girarRoleta() {
-        velocidadeRoleta = 6 + rng.nextDouble()*4;
-        velocidadeBola = 18 + rng.nextDouble()*10;
-        anguloRoleta = rng.nextDouble()*360;
-        anguloBola = rng.nextDouble()*360;
+        if(podeApostar()) {
+            if(apostaMontante!=0) {
+                //interações com jogador
+                this.j.saldo -= this.apostaMontante;
+                this.saldo.setText("SALDO: " + this.j.getSaldo());
+                configurarTexto();
+                this.aposta.setText("SUA APOSTA: 0.0");
 
-        resultado.setText("Roooooodando");
-        resultado.setForeground(new Color(255, 255, 255));
-        girar.setEnabled(false);
+                //desativando os botões
+                aposta5.setEnabled(false);
+                aposta10.setEnabled(false);
+                aposta50.setEnabled(false);
+                zeraAposta.setEnabled(false);
+                allIn.setEnabled(false);
 
-        if(!timer.isRunning()) {
-            timer.start();
+                //física e animação da roleta
+                velocidadeRoleta = 6 + rng.nextDouble()*4;
+                velocidadeBola = 18 + rng.nextDouble()*10;
+                anguloRoleta = rng.nextDouble()*360;
+                anguloBola = rng.nextDouble()*360;
+
+                resultado.setText("Rodando...");
+                resultado.setForeground(new Color(255, 255, 255));
+                girar.setEnabled(false);
+
+                if(!timer.isRunning()) {
+                    timer.start();
+                }
+
+                repaint();
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "APOSTE UM VALOR MAIOR QUE ZERO!");
+            }
         }
-
-        repaint();
+        else {
+            JOptionPane.showMessageDialog(null, "ESCOLHA UM COR PARA APOSTAR!");
+        }
     }
 
     private static double norm360(double d) {
@@ -118,22 +144,214 @@ public class Roleta extends JPanel {
 
             mostrarResultado(numero, cor);
             girar.setEnabled(true);
+            this.j.saldo += calcularGanho(cor);
+            this.apostaMontante = 0.0;
+            configurarTexto();
+
+            //reiniciando a escolha de cor
+            corEscolhida = "0";
+            escolhaCor.setSelectedIndex(0);
+
+            //reativando os botões
+            aposta5.setEnabled(true);
+            aposta10.setEnabled(true);
+            aposta50.setEnabled(true);
+            zeraAposta.setEnabled(true);
+            allIn.setEnabled(true);
         }
         repaint();
     }
 
-    public Roleta() {
+    private void iniciarComponentes() {
+        aposta = new javax.swing.JLabel();
+        saldo = new javax.swing.JLabel();
+        aposta5 = new javax.swing.JButton();
+        aposta10 = new javax.swing.JButton();
+        aposta50 = new javax.swing.JButton();
+        zeraAposta = new javax.swing.JButton();
+        allIn = new javax.swing.JButton();
+
+        //configuração do combobox
+        String[] opcoes = { "Selecione a cor apostada","Vermelho", "Preto", "Verde" };
+        escolhaCor = new javax.swing.JComboBox<>(opcoes);
+        escolhaCor.setFont(new Font("Segoe UI Black", 0, 14));
+        escolhaCor.setBackground(Color.lightGray);
+        escolhaCor.setForeground(Color.BLACK);
+        escolhaCor.setMaximumSize(new java.awt.Dimension(200,16));
+
+        escolhaCor.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED && !e.getItem().equals("Selecione a cor apostada")) {
+                    System.out.println("Cor selecionada: " + e.getItem());
+                    corEscolhida = e.getItem().toString();
+                }
+                else {
+                    corEscolhida = "0";
+                }
+            }
+        });
+
+        //configuração dos labels
+        aposta.setFont(new java.awt.Font("Segoe UI Black", Font.PLAIN, 18));
+        aposta.setForeground(new java.awt.Color(204, 0, 0));
+        aposta.setText("SUA APOSTA: 0.0");
+        aposta.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        aposta.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(new Color(204, 0, 0), 2),
+                BorderFactory.createEmptyBorder(4, 15, 4, 15)
+        ));
+
+        saldo.setFont(new java.awt.Font("Segoe UI Black", Font.PLAIN, 18));
+        saldo.setForeground(new java.awt.Color(255, 204, 0));
+        saldo.setText("SALDO:");
+        saldo.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(new Color(255, 204, 0), 2),
+                BorderFactory.createEmptyBorder(4, 15, 4, 15)
+        ));
+
+        aposta5.setBackground(new java.awt.Color(51, 153, 0));
+        aposta5.setFont(new java.awt.Font("Segoe UI", Font.BOLD, 14));
+        aposta5.setText("5");
+        aposta5.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        aposta5.setPreferredSize(new java.awt.Dimension(25, 25));
+        aposta5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                aposta5ActionPerformed(evt);
+            }
+        });
+
+        aposta10.setBackground(new java.awt.Color(51, 153, 0));
+        aposta10.setFont(new java.awt.Font("Segoe UI", Font.BOLD, 14));
+        aposta10.setText("10");
+        aposta10.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        aposta10.setPreferredSize(new java.awt.Dimension(25, 25));
+        aposta10.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                aposta10ActionPerformed(evt);
+            }
+        });
+
+        aposta50.setBackground(new java.awt.Color(51, 153, 0));
+        aposta50.setFont(new java.awt.Font("Segoe UI", Font.BOLD, 14));
+        aposta50.setText("50");
+        aposta50.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        aposta50.setPreferredSize(new java.awt.Dimension(25, 25));
+        aposta50.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                aposta50ActionPerformed(evt);
+            }
+        });
+
+        zeraAposta.setBackground(new java.awt.Color(255, 255, 0));
+        zeraAposta.setFont(new java.awt.Font("Segoe UI Black", Font.PLAIN, 12));
+        zeraAposta.setText("ZERAR APOSTA");
+        zeraAposta.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        zeraAposta.setPreferredSize(new java.awt.Dimension(110, 25));
+        zeraAposta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                zeraApostaActionPerformed(evt);
+            }
+        });
+
+        allIn.setBackground(new java.awt.Color(204, 0, 0));
+        allIn.setForeground(new Color(35, 35, 35));
+        allIn.setFont(new java.awt.Font("Segoe UI Black", Font.PLAIN, 12));
+        allIn.setText("ALL IN");
+        allIn.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        allIn.setPreferredSize(new java.awt.Dimension(50, 25));
+        allIn.setPreferredSize(new java.awt.Dimension(47, 23));
+        allIn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                allinActionPerformed(evt);
+            }
+        });
+
+    }
+
+    //aposta
+    public void configurarTexto(){
+        this.aposta.setText("SUA APOSTA: " + this.apostaMontante);
+        this.saldo.setText("SALDO: " + this.j.getSaldo());
+    }
+
+    private void verificaApostaMaxima(){
+        if(this.apostaMontante > this.j.getSaldo()){
+            this.apostaMontante = this.j.getSaldo();
+            this.aposta.setText("SUA APOSTA: " + this.apostaMontante);
+        }
+    }
+
+    private boolean podeApostar() {
+        if(corEscolhida.equals("0")) {
+            return false;
+        }
+        return true;
+    }
+
+    private double calcularGanho(String cor) {
+        if(corEscolhida.equalsIgnoreCase(cor)) {
+            if(corEscolhida.equalsIgnoreCase("Verde")) {
+                System.out.println("Verde - "+ this.apostaMontante*36);
+                return this.apostaMontante*36;
+            }
+            else {
+                System.out.println("Preto/Vermelho - "+ this.apostaMontante*2);
+                return this.apostaMontante*2;
+            }
+        }
+        return 0;
+    }
+
+    //ação dos botões
+    private void aposta5ActionPerformed(java.awt.event.ActionEvent evt) {
+        this.apostaMontante += 5;
+        this.aposta.setText("SUA APOSTA: " + this.apostaMontante);
+        verificaApostaMaxima();
+    }
+
+    private void aposta10ActionPerformed(java.awt.event.ActionEvent evt) {
+        this.apostaMontante += 10;
+        this.aposta.setText("SUA APOSTA: " + this.apostaMontante);
+        verificaApostaMaxima();
+    }
+
+    private void aposta50ActionPerformed(java.awt.event.ActionEvent evt) {
+        this.apostaMontante += 50;
+        this.aposta.setText("SUA APOSTA: " + this.apostaMontante);
+        verificaApostaMaxima();
+    }
+
+    private void zeraApostaActionPerformed(java.awt.event.ActionEvent evt) {
+        this.apostaMontante = 0;
+        this.aposta.setText("SUA APOSTA: " + this.apostaMontante);
+    }
+
+    private void allinActionPerformed(java.awt.event.ActionEvent evt) {
+        this.apostaMontante = this.j.getSaldo();
+        this.aposta.setText("SUA APOSTA: " + this.apostaMontante);
+    }
+
+
+    public Roleta(Jogador j) {
+        this.j = j;
+        this.apostaMontante = 0;
+
+        iniciarComponentes();
+        configurarTexto();
         setPreferredSize(new Dimension(720, 950));
         setBackground(new Color(35,35, 35));
 
         timer = new Timer(16, e -> tick());
 
-        resultado = new JLabel("Clique para girar", SwingUtilities.LEFT);
-        resultado.setFont(new Font("SansSerif", Font.BOLD, 20));
+        //config label
+        resultado = new JLabel("Faça sua aposta!", SwingUtilities.LEFT);
+        resultado.setFont(new Font("Segoe UI Black", Font.BOLD, 26));
         resultado.setForeground(Color.WHITE);
-        resultado.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        resultado.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
 
-        girar = new JButton("Girar") {
+        //conf do botão girar
+        girar = new JButton("GIRAR") {
             @Override
             protected void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D)g.create();
@@ -143,16 +361,16 @@ public class Roleta extends JPanel {
                     g2.setColor(new Color(80, 90, 100));
                 }
                 else if(getModel().isPressed()) {
-                    g2.setColor(new Color(60, 130, 200));
+                    g2.setColor(new Color(255, 204, 51));
                 }
                 else if(getModel().isRollover()) {
-                    g2.setColor(new Color(70, 150,220));
+                    g2.setColor(new Color(255, 204, 51));
                 }
                 else {
-                    g2.setColor(new Color(50, 120, 190));
+                    g2.setColor(new Color(255, 204, 51));
                 }
                 g2.fillRoundRect(0, 0, getWidth(), getHeight(), 18, 18);
-                g2.setColor(Color.WHITE);
+                g2.setColor(Color.BLACK);
                 FontMetrics fm = g2.getFontMetrics();
                 int tw = fm.stringWidth(getText());
                 int th = fm.getAscent();
@@ -160,25 +378,73 @@ public class Roleta extends JPanel {
                 g2.dispose();
             }
         };
-
-        girar.setFont(new Font("SansSerif", Font.BOLD, 16));
+        girar.setFont(new Font("Segoe UI Black", Font.PLAIN, 16));
+        girar.setPreferredSize(new Dimension(100,40));
         girar.setContentAreaFilled(false);
         girar.setFocusPainted(false);
         girar.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
         girar.addActionListener(e -> girarRoleta());
 
-        //painel sul
-        JPanel south = new JPanel();
-        south.setLayout(new BoxLayout(south, BoxLayout.Y_AXIS));
-        south.setBackground(new Color(35, 35, 35));
-        south.add(resultado);
-        south.add(Box.createVerticalStrut(10));
-        JPanel btnRow = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
-        btnRow.setOpaque(false);
-        btnRow.add(girar);
-        south.add(btnRow);
+        //Layout
+        //painel esquerda
+        JPanel left = new JPanel();
+        left.setLayout(new BoxLayout(left, BoxLayout.Y_AXIS));
+        left.setBackground(new Color(35, 35, 35));
+
+        JPanel saldoRow = new JPanel(new FlowLayout(FlowLayout.LEFT,40,0));
+        saldoRow.setOpaque(false);
+        saldoRow.add(saldo);
+        left.add(saldoRow);
+        left.add(Box.createVerticalStrut(5));
+
+        JPanel apostaLabelRow = new JPanel(new FlowLayout(FlowLayout.LEFT,40,0));
+        apostaLabelRow.setOpaque(false);
+        apostaLabelRow.add(aposta);
+        left.add(apostaLabelRow);
+        left.add(Box.createVerticalStrut(5));
+
+        JPanel apostasBtRow = new JPanel(new FlowLayout(FlowLayout.LEFT,10,0));
+        apostasBtRow.setOpaque(false);
+        apostasBtRow.add(new JLabel("       "));
+        apostasBtRow.add(zeraAposta);
+        apostasBtRow.add(aposta5);
+        apostasBtRow.add(aposta10);
+        apostasBtRow.add(aposta50);
+        apostasBtRow.add(allIn);
+        left.add(apostasBtRow);
+        left.add(Box.createVerticalStrut(5));
+
+        //painel direita
+        JPanel right = new JPanel();
+        right.setLayout(new BoxLayout(right, BoxLayout.Y_AXIS));
+        right.setBackground(new Color(35, 35, 35));
+
+        JPanel corRow = new JPanel(new FlowLayout(FlowLayout.LEFT,11,30));
+        corRow.setOpaque(false);
+        corRow.add(escolhaCor);
+        right.add(corRow);
+
+        JPanel girarRow = new JPanel(new FlowLayout(FlowLayout.LEFT, 60, 0));
+        girarRow.setOpaque(false);
+        girarRow.add(girar);
+        right.add(girarRow);
+        right.add(Box.createVerticalStrut(20));
 
         setLayout(new BorderLayout());
+
+        //texto superior
+        JPanel resultRow = new JPanel(new FlowLayout(FlowLayout.CENTER,0,0));
+        resultRow.setOpaque(false);
+        resultRow.add(resultado);
+        add(resultRow);
+
+        //painel sul
+        JPanel south = new JPanel();
+        south.setLayout(new BoxLayout(south, BoxLayout.X_AXIS));
+        south.setBackground(new Color(35, 35, 35));
+        south.add(left);
+        south.add(right);
+
         add(south, BorderLayout.SOUTH);
     }
 
@@ -260,13 +526,28 @@ public class Roleta extends JPanel {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() ->{
+            Jogador j = new Jogador();
             JFrame f = new JFrame("Roleta Bonanza");
             f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            f.setContentPane(new Roleta());
+            f.setContentPane(new Roleta(j));
             f.pack();
             f.setLocationRelativeTo(null);
             f.setVisible(true);
             }
         );
     }
+
+    //declaração de variáveis
+    private final JLabel resultado;
+    private final JButton girar;
+    private javax.swing.JButton aposta5;
+    private javax.swing.JButton aposta10;
+    private javax.swing.JButton aposta50;
+    private javax.swing.JButton zeraAposta;
+    private javax.swing.JButton allIn;
+    private JLabel selecionar;
+    private JLabel aposta;
+    private JLabel saldo;
+    private javax.swing.JComboBox<String> escolhaCor;
+    private String corEscolhida = "0";
 }
